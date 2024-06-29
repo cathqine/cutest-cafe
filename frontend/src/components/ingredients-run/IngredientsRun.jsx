def63765bc9f41c37f9d80a92c646f1c4e48aab4
Catherine <z5359881@student.unsw.edu.au>
2024-06-29 19:34:37 +1000

    run ingredients stg1

--- frontend/src/components/ingredients-run/IngredientsRun.jsx
+++ frontend/src/components/ingredients-run/IngredientsRun.jsx
@@ -1,11 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import bg from '../../assets/forest.png';
+import '../../cafe.css';
+import Thing from '../cafe/Thing';
+import { Button, Stack } from '@mui/material';
+
+import flour from '../../assets/flour.png';
+import milk from '../../assets/milk.png';
+import sugar from '../../assets/sugar.png';
+
+import BackButton from '../cafe/BackButton';
 
 const IngredientsRun = () => {
+  const [ing, setIng] = useState('');
+
+  const handleFlourClick = () => {
+    alert('Clicked on flour!');
+    setIng('flour');
+  };
+
+  const handleSugarClick = () => {
+    alert('Clicked on Sugar!');
+    setIng('sugar');
+  };
+
+  const handleMilkClick = () => {
+    alert('Clicked on Milk!');
+    setIng('milk');
+  };
+
+  const handleSubmitClick = () => {
+    alert('Clicked on SUBMIT! collecting: ', ing);
+  };
+
   return (
     <>
-      <header className="mobile">
-        <h1>IngredientsRun</h1>
-      </header>
+      <div className="mobile-forest">
+        <div className='bg-container'>
+          <div className="kitchen-button">
+            <BackButton></BackButton>
+          </div>
+          <img src={bg} alt='forest background' className="forest-bg"></img>
+
+          <div className="ingredients-modal">
+
+            <div className='individual-flour' id='flour' onClick={handleFlourClick}>
+              <Stack direction="row">
+                <img src={flour} alt='flour' className='ing-icons'></img>
+                <div className='ing-txt'>FLOUR</div>
+              </Stack>
+            </div>
+
+            <div className='individual-sugar' id='sugar' onClick={handleSugarClick}>
+              <Stack direction="row">
+                <img src={sugar} alt='flour' className='ing-icons'></img>
+                <div className='ing-txt'>SUGAR</div>
+              </Stack>
+            </div>
+
+            <div className='individual-milk' id='milk' onClick={handleMilkClick}>
+              <Stack direction="row">
+                <img src={milk} alt='flour' className='ing-icons'></img>
+                <div className='ing-txt'>MILK</div>
+              </Stack>
+            </div>
+
+            <div id="submit-to-run" className='individual-submit' onClick={handleSubmitClick}>
+              <div className='ing-txt' style={{ marginLeft: "93px" }}>SUBMIT</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }

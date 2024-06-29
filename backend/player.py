bee513996d916b77561d1bdc68f7fe611cc57004
Adam Zhang <adamzhang.aus@gmail.com>
2024-06-29 14:06:29 +1000

    removed Inventory and moved it into Player

--- backend/player.py
+++ backend/player.py
@@ -1,9 +1,26 @@
-from inventory import Inventory
-
-
 class Player:
     def __init__(self):
         self.levels = 0
         self.experience = 0
         self.money = 0
-        self.inventory = Inventory()
+        self.ingredients = {
+            "flour": 0,
+            "milk": 0,
+            "strawberries": 0,
+            "eggs": 0,
+            "sugar": 0,
+            "nuts": 0,
+            "chocolate": 0,
+        }
+
+    # expects keyword arguments where the values are numbers
+    # the numbers are how many of the respective ingredient was used
+    def use_ingredients(self, **ingredients_used):
+        try:
+            for ingredient, amount_used in ingredients_used.values():
+                self.ingredients[ingredient] -= amount_used
+        # not actually sure what to do if these errors occur
+        except TypeError:
+            return False
+        except KeyError:
+            return False

390ab290d273053ccf0342e75ca2219256ecc1f5
Angeline Tran <angeline.tran@student.unsw.edu.au>
2024-06-29 21:00:06 +1000

    button help

---
+++ frontend/src/components/cafe/KitchenOvenButton.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Tooltip, IconButton } from '@mui/material';
+
+import arrow from '../../assets/outside_arrow.png';
+
+const KitchenOvenButton = (props) => {
+  let location = props.page;
+  if (!props.disabled) {
+    location = props.location;
+  }
+  return (
+    <>
+      <Tooltip title="KitchenOven" arrow>
+        <IconButton>
+          <Link to={location} style={{ textDecoration: 'none' }}>
+            <Button variant='text'>
+              <img src={arrow} width="120px"></img>
+            </Button>
+          </Link>
+        </IconButton>
+      </Tooltip>
+    </>
+  );
+}
+
+export default KitchenOvenButton;
--- frontend/src/components/pages/GettingYourStepsIn.jsx
+++ frontend/src/components/pages/GettingYourStepsIn.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import LeaveWalkButton from '../cafe/LeaveWalkButton';
+import CompleteWalkButton from '../cafe/CompleteWalkButton';
+import '../../cafe.css';
+import bg from '../../assets/forest.png';
 
 const GettingYourStepsIn = () => {
   return (
-    <>
-      <header className="mobile">
-        <p>DisplayCabinet</p>
-      </header>
-    </>
-  )
+    <div className="mobile">
+      {/* level status bar here! */}
+
+      <div className='bg-container'>
+        <img src={bg} alt='cafe background' className="cafe-bg"></img>
+        <div className="leave-walk-button">
+          <LeaveWalkButton></LeaveWalkButton>
+        </div>
+        <div className="complete-walk-button">
+          <CompleteWalkButton></CompleteWalkButton>
+        </div>
+      </div>
+
+    </div>
+  );
 }
 
 export default GettingYourStepsIn;
